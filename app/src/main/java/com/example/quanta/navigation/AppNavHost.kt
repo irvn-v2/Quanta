9684dcb0a1a704711d7049ad2621e79216b754b4
irvn-v2 <irvnmusili@gmail.com>
2024-08-21 11:23:13 +0300

    initial commit

---
+++ app/src/main/java/com/example/quanta/navigation/AppNavHost.kt
@@ -0,0 +1,53 @@
+package com.example.quanta.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.quanta.ui.theme.screens.about.AboutScreen
+import com.example.quanta.ui.theme.screens.home.HomeScreen
+import com.example.quanta.ui.theme.screens.login.LoginScreen
+import com.example.quanta.ui.theme.screens.signup.SignupScreen
+import com.example.quanta.ui.theme.screens.splash.SplashScreen
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination:String = ROUT_SPLASH
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+
+        composable(ROUT_HOME) {
+            HomeScreen(navController = navController)
+        }
+
+
+        composable(ROUT_ABOUT) {
+            AboutScreen(navController = navController)
+        }
+
+        composable(ROUT_SPLASH) {
+            SplashScreen(navController = navController)
+        }
+
+
+        composable(ROUT_SIGNUP) {
+            SignupScreen(navController = navController)
+        }
+
+        composable(ROUT_LOGIN) {
+            LoginScreen(navController = navController)
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/quanta/navigation/Routes.kt
@@ -0,0 +1,7 @@
+package com.example.quanta.navigation
+
+const val ROUT_HOME="com/example/quanta/ui/theme/home"
+const val ROUT_ABOUT="com/example/quanta/ui/theme/screens/about"
+const val ROUT_SPLASH="splash"
+const val ROUT_SIGNUP="signup"
+const val ROUT_LOGIN="login"
